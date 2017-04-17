b4ae94358bc58a07f6ca2eef7a734cf876bcc6b4
KatherineEbel <kathyebel@me.com>
2017-04-17 11:52:01 -0400

    Update UI with weather data

--- app/src/main/java/com/katherineebel/stormy/MainActivity.java
+++ app/src/main/java/com/katherineebel/stormy/MainActivity.java
@@ -1,11 +1,16 @@
 package com.katherineebel.stormy;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.katherineebel.stormy.model.CurrentWeather;
@@ -15,6 +20,8 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
@@ -25,19 +32,50 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private CurrentWeather mCurrentWeather;
+    @BindView(R.id.progressBar)
+    ProgressBar mProgressBar;
+    @BindView(R.id.refreshImageView)
+    ImageView mRefreshImageView;
+    @BindView(R.id.iconImageView)
+    ImageView mIconImageView;
+    @BindView(R.id.temperatureLabel)
+    TextView mTemperatureLabel;
+    @BindView(R.id.humidityValue)
+    TextView mHumidityValue;
+    @BindView(R.id.precipChanceValue)
+    TextView mPrecipChanceValue;
+    @BindView(R.id.timeLabel)
+    TextView mTimeLabel;
+    @BindView(R.id.summaryTextValue)
+    TextView mSummaryTextValue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mProgressBar.setVisibility(View.INVISIBLE);
 
+        mRefreshImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getForecast(37.8267, -122.4233);
+            }
+        });
+
+
+        getForecast(37.8267, -122.4233);
+    }
+
+    private void getForecast(double latitude, double longitude) {
         String apiKey = "bbc404aa1b0ff47e85a6c535d3471b80";
-        double latitude = 37.8267;
-        double longitude = -122.4233;
+//        double latitude = 37.8267;
+//        double longitude = -122.4233;
         String forecastUrl = "https://api.darksky.net/forecast/" + apiKey + "/" + latitude + "," + longitude;
 
         if (isNetworkAvailable()) {
-
+            toggleRefresh();
             OkHttpClient client = new OkHttpClient();
             Request request = new Request.Builder()
                     .url(forecastUrl)
@@ -46,16 +84,34 @@ public class MainActivity extends AppCompatActivity {
             call.enqueue(new Callback() {
                 @Override
                 public void onFailure(Call call, IOException e) {
-
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            toggleRefresh();
+                        }
+                    });
+                    alertUserAboutError();
                 }
 
                 @Override
                 public void onResponse(Call call, Response response) throws IOException {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            toggleRefresh();
+                        }
+                    });
                     try {
                         String jsonData = response.body().string();
                         Log.v(TAG, jsonData);
                         if (response.isSuccessful()) {
                             mCurrentWeather = getCurrentDetails(jsonData);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    updateDisplay();
+                                }
+                            });
                         } else {
                             alertUserAboutError();
                         }
@@ -72,6 +128,26 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "Main UI code is running!");
     }
 
+    private void toggleRefresh() {
+        if (mProgressBar.getVisibility() == View.INVISIBLE) {
+            mProgressBar.setVisibility(View.VISIBLE);
+            mRefreshImageView.setVisibility(View.INVISIBLE);
+        } else {
+            mProgressBar.setVisibility(View.INVISIBLE);
+            mRefreshImageView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void updateDisplay() {
+        mTemperatureLabel.setText(mCurrentWeather.getTemperature() + "");
+        mTimeLabel.setText("At " + mCurrentWeather.getFormattedtime() + "" + " it will be:");
+        mHumidityValue.setText(mCurrentWeather.getHumidity() + "");
+        mPrecipChanceValue.setText(mCurrentWeather.getPrecipChance() + " %");
+        mSummaryTextValue.setText(mCurrentWeather.getSummary());
+        Drawable drawable = getResources().getDrawable(mCurrentWeather.getIconId());
+        mIconImageView.setImageDrawable(drawable);
+    }
+
     private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
         JSONObject forecast = new JSONObject(jsonData);
         JSONObject currently = forecast.getJSONObject("currently");
@@ -81,7 +157,7 @@ public class MainActivity extends AppCompatActivity {
                 currently.getLong("time"),
                 currently.getDouble("temperature"),
                 currently.getDouble("humidity"),
-                currently.getDouble("precipProbablility"),
+                currently.getDouble("precipProbability"),
                 currently.getString("summary"),
                 forecast.getString("timezone"));
         return weather;
--- app/src/main/java/com/katherineebel/stormy/model/CurrentWeather.java
+++ app/src/main/java/com/katherineebel/stormy/model/CurrentWeather.java
@@ -1,6 +1,8 @@
 package com.katherineebel.stormy.model;
 
 
+import com.katherineebel.stormy.R;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
@@ -24,6 +26,7 @@ public class CurrentWeather {
         this.mTemperature = temperature;
         this.mHumidity = humidity;
         this.mPrecipChance = precipChance;
+        this.mTimeZone = timeZone;
         this.mSummary = summary;
     }
 
@@ -31,6 +34,32 @@ public class CurrentWeather {
         return mIcon;
     }
 
+    public int getIconId() {
+        switch (mIcon) {
+            case "clear-day":
+                return R.drawable.clear_day;
+            case "clear-night":
+                return R.drawable.clear_night;
+            case "rain":
+                return R.drawable.rain;
+            case "snow":
+                return R.drawable.snow;
+            case "sleet":
+                return R.drawable.sleet;
+            case "wind":
+                return R.drawable.wind;
+            case "fog":
+                return R.drawable.fog;
+            case "cloudy":
+                return R.drawable.cloudy;
+            case "partly-cloudy-day":
+                return R.drawable.partly_cloudy;
+            case "partly-cloudy-night":
+                return R.drawable.cloudy_night;
+            default:
+                return R.drawable.clear_day;
+        }
+    }
     public void setIcon(String icon) {
         this.mIcon = icon;
     }
@@ -50,8 +79,8 @@ public class CurrentWeather {
         this.mTime = time;
     }
 
-    public double getTemperature() {
-        return mTemperature;
+    public int getTemperature() {
+        return (int) Math.round(mTemperature);
     }
 
     public void setTemperature(double temperature) {
@@ -66,8 +95,8 @@ public class CurrentWeather {
         this.mHumidity = humidity;
     }
 
-    public double getPrecipChance() {
-        return mPrecipChance;
+    public int getPrecipChance() {
+        return (int) Math.round(mPrecipChance * 100);
     }
 
     public void setPrecipChance(double precipChance) {
